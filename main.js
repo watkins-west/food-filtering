7959fd23705dc07dbb355521567477d8760ae70f
David Westenhaver <david.westenhaver74@journalism.cuny.edu>
2022-03-08 20:23:05 -0500

    Complete Version with Opacity

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // silence 
+const select = document.querySelector('.filter');
+
+select.addEventListener('change', function (e) {
+    const chosen_food = e.target.value;
+    const currently_visible = document.querySelectorAll('.item.active')
+    const make_visible = document.querySelectorAll(`.item.${chosen_food}`)
+    
+    currently_visible.forEach(function (food) {
+        food.classList.remove('active');
+
+    make_visible.forEach(function (food) {
+        food.classList.add('active');
+    });
+        
+    });
+
+});
